dffe27ec2eefac2eb7c35b24a9bec92f5f1c09e5
AGauff <94204057+AGauff@users.noreply.github.com>
2026-04-28 18:35:24 +0300

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,32 @@
 import Hero from './components/Hero'
-import About from './components/About'
-import Partners from './components/Partners'
-import Audience from './components/Audience'  
-import Programs from './components/Programs'
-import Speakers from './components/Speakers'
-import Results from './components/Results'
-import Formats from './components/Formats'
-import Contacts from './components/Contacts'
-import Resources from './components/Resources'
 import Problem from './components/Problem'
+import Audience from './components/Audience'
+import Programs from './components/Programs'
+import Differentiator from './components/Differentiator'
+import Results from './components/Results'
+import Proof from './components/Proof'
+import About from './components/About'
+import Speakers from './components/Speakers'
+import Process from './components/Process'
+import Formats from './components/Formats'
+import FAQ from './components/FAQ'
+import Contacts from './components/Contacts'
 
 export default function Home() {
   return (
     <main>
       <Hero />
-      <About />
-      <Partners />  
-      <Resources />
-      <Speakers />
       <Problem />
       <Audience />
       <Programs />
+      <Differentiator />
       <Results />
+      <Proof />
+      <About />
+      <Speakers />
+      <Process />
       <Formats />
+      <FAQ />
       <Contacts />
     </main>
   )
